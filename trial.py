9e0f7bc17008538fb1d79ed8c12d2d7c2219677f
Caroline <bancroftwanjiru10000@gmail.com>
2024-09-15 18:51:17 +0300

    intro to SQL

---
+++ trial.py
@@ -0,0 +1,23 @@
+'''
+use SQL to store and retrieve data 
+use ti to build relational database on your computer 
+perfor CRUD operations on relationals databases using SQL
+
+
+
+SQL Means structures query language- it is a programming language that us used to manage rationale databases and perform operations in the data that they contain 
+rational database is a collections of data that is organized in well efined relationships
+qiery is a atatement that is used to return data from databases
+sql is a data specific programming language.
+
+with data base management skills wehll lean to store representations of the oython objects ounprogram creates and retrieve thatm at the appropriate time.
+well also learn how to connect out python and applcations to our databases.
+
+
+create a sqllite3  databasess.
+create update and select and delete data fromdatabses tables 
+relate data within the given database.
+write sql code in both your command line and your text editor and execute thecode against a database.
+write python progrmas that talk to and save data to your databases.
+
+''' 
